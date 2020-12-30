79ccdd7d4d48175b7bd5c11a3afc28802d1eea00
Andrea Manzini <ilmanzo@gmail.com>
2020-12-30 12:06:05 +0100

    str-comb crystal implementation (fixed)

---
+++ str-comb/scomb.cr
@@ -0,0 +1,31 @@
+##############################
+# scomb.cr
+def newGap(gap)
+  gap /= 1.3
+  gap = gap.to_i
+  return 11 if gap == 9 || gap == 10
+  return 1 if gap < 1
+  gap
+end
+
+def combSort(a)
+  len = a.size
+  gap = len
+  swapped = false
+  loop do
+    swapped = false
+    gap = newGap gap
+    (0...(len - gap)).each do |i|
+      if a[i] > a[i + gap]
+        swapped = true
+        a[i], a[i + gap] = a[i + gap], a[i]
+      end
+    end
+    break if gap <= 1 && !swapped
+  end
+end
+
+N = 10000000
+arr = N.times.map { |x| (N - x).to_s }.to_a
+combSort arr
+(1...N).each { |z| print '!' if arr[z] < arr[z - 1] }
