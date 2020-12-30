49b3379a8745608943039a8e19c64c524c2217bc
Andrea Manzini <ilmanzo@gmail.com>
2020-12-30 17:40:32 +0100

    comb implementation for nim

--- str-comb/scomb.go
+++ str-comb/scomb.go
@@ -1,46 +1,52 @@
 //////////////////////////////
 // scomb.go
 package main
-import "fmt"
-import "strconv"
+
+import (
+	"fmt"
+	"strconv"
+)
+
 func newGap(gap int) int {
-    gap = int(float64(gap) / 1.3)
-    if gap == 9 || gap == 10 {
-        return 11
-    }
-    if gap < 1 {
-        return 1
-    }
-    return gap
+	gap = int(float64(gap) / 1.3)
+	if gap == 9 || gap == 10 {
+		return 11
+	}
+	if gap < 1 {
+		return 1
+	}
+	return gap
 }
 func combSort(a []string) {
-    xlen := len(a)
-    gap := xlen
-    swapped := false
-    for {
-        swapped = false
-        gap = newGap(gap)
-        for i := 0; i < xlen-gap; i++ {
-            if a[i] > a[i+gap] {
-                swapped = true
-                a[i], a[i+gap] = a[i+gap], a[i]
-            }
-        }
-        if !(gap > 1 || swapped) {
-            break
-        }
-    }
+	xlen := len(a)
+	gap := xlen
+	swapped := false
+	for {
+		swapped = false
+		gap = newGap(gap)
+		for i := 0; i < xlen-gap; i++ {
+			if a[i] > a[i+gap] {
+				swapped = true
+				a[i], a[i+gap] = a[i+gap], a[i]
+			}
+		}
+		if !(gap > 1 || swapped) {
+			break
+		}
+	}
 }
+
 const N = 10000000
+
 func main() {
-    arr := make([]string, N, N)
-    for z := 0; z < N; z++ {
-        arr[z] = strconv.Itoa(N - z)
-    }
-    combSort(arr)
-    for z := 1; z < N; z++ {
-        if arr[z] < arr[z-1] {
-            fmt.Print("!")
-        }
-    }
+	arr := make([]string, N, N)
+	for z := 0; z < N; z++ {
+		arr[z] = strconv.Itoa(N - z)
+	}
+	combSort(arr)
+	for z := 1; z < N; z++ {
+		if arr[z] < arr[z-1] {
+			fmt.Print("!")
+		}
+	}
 }
--- str-comb/scomb.py
+++ str-comb/scomb.py
@@ -20,7 +20,7 @@ def combSort(a, len):
                 a[i+gap] = temp
         if not(gap > 1 or swapped):
             break
-N = 10000000;
+N = 10000000
 arr = [ str(N-z) for z in range(0,N)] 
 combSort(arr,N)
 for z in range(1,N):
