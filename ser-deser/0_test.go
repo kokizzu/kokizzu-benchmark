1c5e1f2c0403201b46e48207f23a00ac95575bc1
Kiswono Prayogo <kiswono@gmail.com>
2022-12-21 19:01:23 +0700

    remove failed attempt xdr2

--- ser-deser/0_test.go
+++ ser-deser/0_test.go
@@ -180,12 +180,4 @@ func TestVerify(t *testing.T) {
 		PelletierGoTomlV2_MarshalUnmarshal(resultA, &resultB)
 		mapMatch(t, resultB)
 	})
-	//t.Run("DavecghGoXdrXdr2_MarshalUnmarshal", func(t *testing.T) {
-	//	resultA = myStruct{}
-	//	DavecghGoXdrXdr2_MarshalUnmarshal(myMap1, &resultA)
-	//	structMatch(t, &resultA)
-	//	resultB = map[string]any{}
-	//	DavecghGoXdrXdr2_MarshalUnmarshal(resultA, &resultB)
-	//	mapMatch(t, resultB)
-	//})
 }
